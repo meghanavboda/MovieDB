c3104476f31db7e54b0048f828c257925fbc9c38
meghanavboda <meghanavboda@gmail.com>
2025-06-05 19:51:19 -0400

    Initial App + Search Function

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import Search from "./components/Search";
+
+function App() {
+  const[state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=50b647de";
+
+  const handleInput = (e) => {
+      let s = e.target.value;
+
+      setState(prevState => {
+        return{...prevState, s: s}
+      });
+
+      console.log(state.s);
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>Movie Database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput}/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Search({handleInput}) {
+  return (
+    <section className="searchbox-wrap">
+        <input 
+            type="text" 
+            placeholder="Search for a movie..." 
+            className='searchbox' 
+            onChange={handleInput} 
+        />
+    </section>
+  )
+}
+
+export default Search
